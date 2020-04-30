ebaa8398ff2c26ef041879ca91ca5a915da0d36f
David Hong <dahong67@wharton.upenn.edu>
2020-04-30 11:03:03 -0400

    Copy previous SAGE code, add some tests and benchmarks.

---
+++ benchmark/benchmarks.jl
@@ -0,0 +1,24 @@
+using BenchmarkTools, HeteroscedasticPCA
+using Random, LinearAlgebra, BlockArrays
+
+const SUITE = BenchmarkGroup()
+
+rng = MersenneTwister(123)
+
+# PPCA
+SUITE["sage"] = BenchmarkGroup()
+nfull, vfull = (10, 40), (1, 4)
+for d = 5:10:15, k = 1:3:4, L = 1:2
+    n, v = nfull[1:L], vfull[1:L]
+    F, Z = randn(rng, d, k), [randn(rng, k, nl) for nl in n]
+    Y = [F * Zl + sqrt(vl) * randn(rng, d, nl) for (Zl, vl, nl) in zip(Z, v, n)]
+
+    Yflat = hcat(Y...)
+    Yblock = BlockArray(Yflat, [d], collect(n))
+    F0 = randn(rng, d, k)
+
+    for T = 0:10:20
+        SUITE["sage"]["block: d=$d, k=$k, L=$L, T=$T"] = @benchmarkable HeteroscedasticPCA.ppca($Yblock, $k, $T, $F0)
+        SUITE["sage"]["flat:  d=$d, k=$k, L=$L, T=$T"] = @benchmarkable HeteroscedasticPCA.ppca($Yflat, $k, $T, $F0)
+    end
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,40 @@
 using HeteroscedasticPCA
 using Test
 
-@testset "HeteroscedasticPCA.jl" begin
-    # Write your tests here.
+module Reference
+include("ref/alg,sage.jl")
+end
+
+using Random, LinearAlgebra, BlockArrays
+
+rng = MersenneTwister(123)
+
+@testset "alg,sage.jl" begin
+    nfull, vfull = (10, 40), (1, 4)
+    for d = 5:10:25, k = 1:3, L = 1:2
+        @testset "d=$d, k=$k, L=$L" begin
+            n, v = nfull[1:L], vfull[1:L]
+            F, Z = randn(rng, d, k), [randn(rng, k, nl) for nl in n]
+            Y = [F * Zl + sqrt(vl) * randn(rng, d, nl) for (Zl, vl, nl) in zip(Z, v, n)]
+
+            Yflat = hcat(Y...)
+            Yblock = BlockArray(Yflat, [d], collect(n))
+
+            F0 = randn(rng, d, k)
+
+            @testset "block" begin
+                Fhat, vhat = HeteroscedasticPCA.ppca(Yblock, k, 10, F0)
+                Fref, vref = Reference.SAGE.ppca(Yblock, k, 10, F0)
+                @test Fhat == Fref
+                @test vhat == vref
+            end
+
+            @testset "flat" begin
+                Fhat, vhat = HeteroscedasticPCA.ppca(Yflat, k, 10, F0)
+                Fref, vref = Reference.SAGE.ppca(Yflat, k, 10, F0)
+                @test Fhat == Fref
+                @test vhat == vref
+            end
+        end
+    end
 end
