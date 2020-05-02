104babb5c3b87312a6225293d1bf7e707fc74372
David Hong <dahong67@wharton.upenn.edu>
2020-05-02 18:33:45 -0400

    Add some comments

--- src/HeteroscedasticPCA.jl
+++ src/HeteroscedasticPCA.jl
@@ -16,9 +16,9 @@ using Logging
 
 # Types
 struct HPPCA{T<:AbstractFloat}
-    U::Matrix{T}
-    λ::Vector{T}
-    v::Vector{T}
+    U::Matrix{T}   # eigvecs of FF'
+    λ::Vector{T}   # eigvals of FF'
+    v::Vector{T}   # block noise variances
     function HPPCA{T}(U,λ,v) where {T<:AbstractFloat}
         size(U,2) == length(λ) || throw(DimensionMismatch("U has dimensions $(size(U)) but λ has length $(length(λ))"))
         new{T}(U,λ,v)
