2e6691f1c89dfde56bf58c1d5799de383342d1e4
Javier Salazar Cavazos <114938096+javiersc-umich@users.noreply.github.com>
2022-10-04 16:49:05 -0400

    Update src/extern.jl
    Co-authored-by: David Hong <dahong67@users.noreply.github.com>

--- src/extern.jl
+++ src/extern.jl
@@ -15,7 +15,7 @@ Estimate probabilistic PCA model for noise that is heteroscedastic across sample
 + `varfloor` : lower bound for variance iterates (useful if the iterates are degenerating to zero) default = 0
 Output is a [`HePPCATModel`](@ref) object.
 """
-function heppcat(Y,k,iters::Integer;init=homppca(Y,k),vknown::Bool=false,varfloor::Real=0.0)
+function heppcat(Y,k,iters::Integer;init=homppca(Y,k),vknown::Bool=false,varfloor=zero(eltype(first(Y))))
     M = init
     for _ in 1:iters
         vknown || updatev!(M,Y,ProjectedVariance(ExpectationMaximization(),varfloor))
